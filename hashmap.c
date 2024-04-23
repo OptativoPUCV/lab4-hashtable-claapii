4c0ebf6df0cf32fa959e28b306426779d3cc1b3b
Diego Valenzuela <163453007+claapii@users.noreply.github.com>
2024-04-23 14:02:31 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -59,6 +59,7 @@ void insertMap(HashMap * map, char * key, void * value)
 
 void enlarge(HashMap * map) 
 {
+  if (map == NULL) return;
   enlarge_called = 1; //no borrar (testing purposes)
   Pair ** aux = map->buckets;
   map->capacity *= 2;
