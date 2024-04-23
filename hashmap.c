d73f0c47512342bb8ec8112e6204f2738168caef
Diego Valenzuela <163453007+claapii@users.noreply.github.com>
2024-04-23 01:36:20 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -73,7 +73,7 @@ Pair * searchMap(HashMap * map,  char * key) {
   while(par != NULL && par->key != NULL && is_equal(par->key, key) == 0)
   {
     par = map->buckets[posicion+1];
-    map->current = par;
+    map->current = map->buckets[posicion+1] ;
   }
   return par;
 }
