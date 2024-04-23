178d4ea6f87bcccdab622dc739b38c03659c56f1
Diego Valenzuela <163453007+claapii@users.noreply.github.com>
2024-04-23 01:40:36 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -68,13 +68,7 @@ void eraseMap(HashMap * map,  char * key)
 }
 
 Pair * searchMap(HashMap * map,  char * key) {   
-  long posicion = hash(key,map->capacity);
-  Pair *par = map->buckets[posicion];
-  while(par != NULL && par->key != NULL && is_equal(par->key, key) == 0)
-  {
-    
-  }
-  return par;
+  
 }
 
 Pair * firstMap(HashMap * map) {
