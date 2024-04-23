6247c73e5a4d195e61d8617b3b3f4c2af1676ea7
Diego Valenzuela <163453007+claapii@users.noreply.github.com>
2024-04-23 02:32:05 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -116,7 +116,7 @@ Pair * firstMap(HashMap * map)
   if (map == NULL) return NULL;
   for (long i = 0; i < map->capacity; i++)
   {
-    if (map->buckets[i] != NULL)
+    if (map->buckets[i] != NULL && map->buckets[i]->key != NULL)
     {
       map->current = i;
       return map->buckets[i];
